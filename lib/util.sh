47b7c8f3612f39b5de10850dd9d9f5dae5722d1f
Bernhard Landauer <oberon@manjaro.org>
2016-10-25 18:46:27 +0200

    upd netgroups

--- lib/util.sh
+++ lib/util.sh
@@ -456,7 +456,7 @@ load_profile_config(){
 
     [[ -z ${chrootcfg} ]] && chrootcfg='false'
 
-    [[ -z ${netgroups} ]] && netgroups="https://raw.githubusercontent.com/manjaro/manjaro-tools-iso-profiles/master/shared/netinst"
+    [[ -z ${netgroups} ]] && netgroups="https://raw.githubusercontent.com/manjaro/iso-profiles/master/shared/netgroups"
 
     [[ -z ${geoip} ]] && geoip='true'
 
